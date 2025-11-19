1d3a9b93eb50d53ea3c032275ceafec4a7b03305
Kepha-M <thecloud79@gmail.com>
2025-11-19 21:19:22 +0300

    upgrade to work with Render

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,7 @@
-import uvicorn
+from fastapi import FastAPI
 
-if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "backend is running"}
