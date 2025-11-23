bd81281d9eaaa0aff86eb9ad46abe744be3d67bc
Kepha-M <thecloud79@gmail.com>
2025-11-23 14:40:11 +0300

    application of context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { HashRouter } from "react-router-dom";
+import AuthProvider from "./context/AuthContext"; // import your provider
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <AuthProvider> {/* Wrap the entire app in AuthProvider */}
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
-// Using HashRouter to ensure compatibility with GitHub Pages routing
