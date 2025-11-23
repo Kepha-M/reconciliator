22d992010e2fa977be65eaf3bd7593933dfc2f8d
Kepha-M <thecloud79@gmail.com>
2025-11-23 13:48:24 +0300

    new migrations

--- migrations/env.py
+++ migrations/env.py
@@ -2,15 +2,9 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
+from app.models import Base 
 from alembic import context
 
-import sys
-import os
-
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
-from app.models.models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
